fecf65b1b49737ec450024b8d9eb59829354d7ab
suitflick <suitflick0003@gmail.com>
2026-07-13 13:39:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ console.log("Button clicked");
 });
 
 });
+// SuitFlick JavaScript
+
+console.log("SuitFlick Loaded Successfully");
+
+// Shop Now Button
+document.querySelectorAll("button").forEach(button => {
+  button.addEventListener("click", function () {
+    console.log(button.innerText + " clicked");
+  });
+});
