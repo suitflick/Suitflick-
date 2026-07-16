eb841df962ec0028491a3e46e86c9a6cc354893f
suitflick <suitflick0003@gmail.com>
2026-07-16 18:14:50 +0530

    Create checkout.js

---
+++ checkout.js
@@ -0,0 +1,81 @@
+import { db, auth } from "./firebase.js";
+
+import {
+collection,
+addDoc,
+serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (e) => {
+
+e.preventDefault();
+
+const user = auth.currentUser;
+
+if (!user) {
+alert("Please Login First");
+window.location.href = "account.html";
+return;
+}
+
+const name = document.querySelector('input[type="text"]').value;
+
+const mobile = document.querySelector('input[type="tel"]').value;
+
+const email = document.querySelector('input[type="email"]').value || user.email;
+
+const address = document.querySelector("textarea").value;
+
+const pincode = document.querySelector('input[type="number"]').value;
+
+const payment = document.querySelector("select").value;
+
+const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+let total = 0;
+
+cart.forEach(item => {
+total += Number(item.price);
+});
+
+try {
+
+await addDoc(collection(db, "orders"), {
+
+userEmail: user.email,
+
+name: name,
+
+mobile: mobile,
+
+email: email,
+
+address: address,
+
+pincode: pincode,
+
+paymentMethod: payment,
+
+products: cart,
+
+totalPrice: total,
+
+createdAt: serverTimestamp()
+
+});
+
+alert("🎉 Order Placed Successfully!");
+
+localStorage.removeItem("cart");
+
+window.location.href = "index.html";
+
+} catch (error) {
+
+alert(error.message);
+
+}
+
+});
